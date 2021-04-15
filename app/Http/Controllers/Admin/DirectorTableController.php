1cff72fd94e762e0e00226cfd084bfc00b9f3933
lucaslidorio <lucaslidorio@hotmail.com>
2021-04-14 21:42:14 -0400

    mesa diretora

--- app/Http/Controllers/Admin/DirectorTableController.php
+++ app/Http/Controllers/Admin/DirectorTableController.php
@@ -3,19 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateDirectorTable;
 use App\Models\Biennium;
+use App\Models\Councilor;
 use App\Models\DirectorTable;
+use App\Models\DirectorTableMemberFunctions;
+use App\Models\Functions;
 use Illuminate\Http\Request;
 use PhpParser\Node\Expr\FuncCall;
 
 class DirectorTableController extends Controller
 {
-    private $repository, $biennium;
+    private $repository, $biennium, $councilor, $function;
 
-    public function __construct(DirectorTable $directorTable, Biennium $biennium)
+    public function __construct(DirectorTable $directorTable, Biennium $biennium,
+        Councilor $councilor, Functions $function)
     {
         $this->repository = $directorTable;
         $this->biennium = $biennium;
+        $this->councilor = $councilor;
+        $this->function = $function;
     }
     public function index()
     {
@@ -36,53 +43,94 @@ class DirectorTableController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreUpdateDirectorTable $request)
     {
-        //
+        $this->repository->create($request->all());
+        toast('Cadastro realizado com sucesso!','success')->toToast('top') ;     
+        return redirect()->back();
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
-        //
+        $bienniuns = $this->biennium->orderBy('id', 'DESC')->get(); 
+        $directorTable = $this->repository->where('id', $id)->first();
+       if (!$directorTable) {
+           redirect()->back();
+       }
+       return view('admin.pages.directorTables.edit', compact('directorTable', 'bienniuns'));
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+       public function update(StoreUpdateDirectorTable $request, $id)
     {
-        //
+        $directorTable = $this->repository->where('id', $id)->first();
+        
+        if (!$directorTable) {
+            redirect()->back();
+        }  
+
+        $directorTable->update($request->all());
+        toast('Mesa Diretora atualizada com sucesso!','success')->toToast('top');            
+        return redirect()->route('directorTables.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function destroy($id)
     {
-        //
+        
+        $directorTable = $this->repository->where('id', $id)->first();
+        
+        if (!$directorTable) {
+            redirect()->back();
+        }
+
+        $directorTable->delete();
+        toast('Mesa Diretoraexcluida com sucesso!','success')->toToast('top');            
+        return redirect()->route('directorTables.index');
     }
+
+    
+    public function search (Request $request){
+
+        $pesquisar = $request->except('_token');
+        $directorTable = $this->repository->search($request->pesquisa);
+
+        return view('admin.pages.directorTables.index', [
+            'directorTables' =>$directorTable,
+            'pesquisar' =>$pesquisar
+        ]);
+    }
+
+
+    //Inicio do crud de membros das comissões
+    public function members($id){
+        
+        
+        $dataTable= DirectorTableMemberFunctions::with('members', 'functions')->where('director_table_id', $id)->get();          
+        $directorTable = $this->repository->where('id', $id)->first();
+         
+        return view('admin.pages.directorTables.members.index', [
+            'dataTable' => $dataTable,
+            'directorTable' => $directorTable
+        
+     ]);
+    }
+    public function membersCreate($id){
+        
+        $directorTable = $this->repository->where('id', $id)->first();
+        $councilors = $this->councilor->get();
+        $functions = $this->function->get();
+        return view('admin.pages.directorTables.members.create', [
+            
+            'directorTable' => $directorTable,
+            'councilors' => $councilors,
+            'functions' => $functions,           
+        ]);      
+    }
+
 }
