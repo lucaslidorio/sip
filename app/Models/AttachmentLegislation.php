f8521a2f49b90e3f511a9c6efdf9ad6e976050fb
Lucas Lidorio <lucaslidorio@hotmail.com>
2022-06-26 19:54:47 -0400

    push vs

--- app/Models/AttachmentLegislation.php
+++ app/Models/AttachmentLegislation.php
@@ -8,8 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class AttachmentLegislation extends Model
 {
     use HasFactory;
-    
-   
+    //  
     protected $table = 'attachment_legislations';
     protected $fillable = ['user_id', 'legislation_id', 'type_document_id', 'anexo', 'nome_original'];
     
