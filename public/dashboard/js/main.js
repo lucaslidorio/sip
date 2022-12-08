3b9978b6a6b3f13b280d9763e650aac4c779d888
Lucas Lidorio <lucaslidorio@hotmail.com>
2022-12-08 10:36:38 -0400

    ajuste select2

--- public/dashboard/js/main.js
+++ public/dashboard/js/main.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
   $(".telefone_celular").inputmask('(99) 9999[9]-9999');
   $(".cpf").inputmask("999.999.999-99");
   $(".cnpj").inputmask("99.999.999/9999-99");
-
   
   //Inicia o plugin select2 na onde possuir a classe select2
   //desabilitado
