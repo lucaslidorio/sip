e1c07a8fbf0de10bf4871c323d7006e63b1af754
Lucas Lidorio <lucaslidorio@hotmail.com>
2023-10-13 11:15:51 -0400

    antes de mudar versão s3

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,29 +42,31 @@ return [
             'visibility' => 'public',
         ],
 
-        'minio' => [
-            'driver' => 's3',
-            'endpoint' => env('MINIO_ENDPOINT'),
-            'key' => env('MINIO_ACCESS_KEY'),
-            'secret' => env('MINIO_SECRET_KEY'),
-            'region' => 'us-east-1', // Substitua pela sua região, se necessário
-            'bucket' => env('MINIO_BUCKET'),
-        ],
-
-
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'version'     => "latest",
-            'bucket_endpoint' => false,
-            'use_path_style_endpoint' => true
+            'endpoint' => env('AWS_URL'),
+            'use_path_style_endpoint' => true,
+        ]
 
-        ],
+
+
+        // 's3' => [
+        //     'driver' => 's3',
+        //     'key' => env('AWS_ACCESS_KEY_ID'),
+        //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        //     'region' => env('AWS_DEFAULT_REGION'),
+        //     'bucket' => env('AWS_BUCKET'),
+        //     'url' => env('AWS_URL'),
+        //     'endpoint' => env('AWS_ENDPOINT'),
+        //     'version'     => "latest",
+        //     'bucket_endpoint' => false,
+        //     'use_path_style_endpoint' => true
+
+        // ],
 
     ],
 
