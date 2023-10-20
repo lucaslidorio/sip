56ae3227f567714cb9279ca03225143d58e36499
Lucas Lidorio <lucaslidorio@hotmail.com>
2023-10-19 21:06:28 -0400

    limpeza

--- resources/views/site/layouts-old/message.blade.php
+++
@@ -1,6 +0,0 @@
-<h2>Você recebeu um novo e-mail</h2>
-
-<p> <strong>Nome:</strong>{{$data ['name']}} </p>
-<p> <strong>E-mail:</strong>{{$data ['email']}} </p>
-<p> <strong>Assunto:</strong>{{$data ['assunto']}} </p>
-<p> <strong>Mensagem:</strong>{{$data ['mensagem']}} </p>
