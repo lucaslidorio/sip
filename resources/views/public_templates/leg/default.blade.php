3d8b6e4e9ae9dfab00653747f05c5b670c90066a
Lucas Lidorio <lucaslidorio@hotmail.com>
2025-03-31 21:30:51 -0400

    ajuste no titulo

--- resources/views/public_templates/leg/default.blade.php
+++ resources/views/public_templates/leg/default.blade.php
@@ -5,12 +5,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Prefeitura Municipal de Teixeiropolis/RO">
+    <meta name="description" content="{{$tenant->nome}}">
     <title>{{$tenant->nome}}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3..2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-     --}}
+   
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         
