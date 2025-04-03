dac1cf7fa88e653c72c592eddff720723ee528d2
Lucas Lidorio <lucaslidorio@hotmail.com>
2025-04-03 00:02:00 -0400

    ajuste em vereadores da index

--- resources/views/public_templates/leg/includes/vereadores.blade.php
+++ resources/views/public_templates/leg/includes/vereadores.blade.php
@@ -11,38 +11,39 @@
 
         <div class="slide-vereadores box-centralizada">
             <div class="highlights2-texts">
-                @if($vereadores->isNotEmpty())
-                @foreach($vereadores->chunk(4) as $chunk)
-                <div class="container">
-                    <div class="row">
-                        @foreach($chunk as $vereador)
-                        <div class="col">
-                            <img style="width:100%; height:280px" class="img-fluid" src="{{ config('app.aws_url') . "
-                                {$vereador->img}" }}"
-                            alt="{{ $vereador->nome }}" class="img-responsive" />
-                            <section>
-                                <a href="{{ route('camara.vereador', $vereador->id) }}" role="button">
-                                    <h4>
-                                        <div style="width: 100%;" class="col-md-1 col-sm-1 col-xs-1">
-                                            <div style="width: 80px; float: left;">
-                                                <img width="50px" src="{{ config('app.aws_url')."
-                                                    {$vereador->party->img}" }}"
-                                                alt="">
-                                            </div>
-                                            <div style="width: 70%; padding-top: 3%; float: right; color: #666;">
-                                                {{ $vereador->nome }}
-                                            </div>
-                                        </div>
-                                    </h4>
-                                </a>
-                            </section>
-                        </div>
-                        @endforeach
-                    </div>
-                </div>
-                @endforeach
-                @endif
 
+    @foreach($vereadores->chunk(4) as $chunk)
+        <div class="container">
+            <div class="row">
+                @foreach($chunk as $vereador)
+                    <div class="col">
+                        <img style="width:100%; height:280px" class="img-fluid"
+                            src="{{ config('app.aws_url') . "{$vereador->img}" }}"
+                            alt="{{ $vereador->nome }}" class="img-responsive" />
+                        <section>
+                            <a href="{{ route('camara.vereador', $vereador->id) }}" role="button">
+                                <h4>
+                                    <div style="width: 100%;" class="col-md-1 col-sm-1 col-xs-1">
+                                        <div style="width: 80px; float: left;">
+                                            <img width="50px"
+                                                src="{{ config('app.aws_url')."{$vereador->party->img}" }}"
+                                                alt="">
+                                        </div>
+                                        <div
+                                            style="width: 70%; padding-top: 3%; float: right; color: #666;">
+                                            {{ $vereador->nome }}
+                                        </div>
+                                    </div>
+                                </h4>
+                            </a>
+                        </section>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    @endforeach
+@endif
+                
 
             </div>
 
@@ -50,4 +51,3 @@
         <div class="highlights2-dots"></div>
     </section>
 </div>
-@endif
